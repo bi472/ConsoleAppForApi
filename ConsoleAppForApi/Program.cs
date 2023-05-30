7d61f142a8da9bb4d4e9495199c16cc52fd8d5ef
Aidar <matt.nolimiteron@gmail.com>
2023-05-31 00:07:06 +0300

    чуть-чуть изменил

--- ConsoleAppForApi/Program.cs
+++ ConsoleAppForApi/Program.cs
@@ -1,41 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Newtonsoft;
 
 class Program
 {
     static async Task Main()
     {
-        
-        using (HttpClient client = new HttpClient())
-        {
-            string text = "Это очень большой большой текст.";
-            
-            HttpContent content = new StringContent(text);
+        // Создаем HttpClient
+        var client = new HttpClient();
+        var request = new HttpRequestMessage(HttpMethod.Post, "http://localhost:5154/Process");
+        var content = new StringContent("\r\n\"lkasdlksa;ldkl;askd; k;lksa;kd klsa;dklsd\"\r\n", null, "application/json");
+        request.Content = content;
+        var response = await client.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+        Console.WriteLine(await response.Content.ReadAsStringAsync());
 
-            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
-
-            HttpResponseMessage response = await client.PostAsync("https://localhost:7049/Process", content);
-
-            if (response.IsSuccessStatusCode)
-            {
-                string jsonResponse = await response.Content.ReadAsStringAsync();
-
-                // Десериализуем JSON в словарь
-                Dictionary<string, int> responseData = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, int>>(jsonResponse);
-
-                // Выводим полученные данные
-                foreach (KeyValuePair<string, int> item in responseData)
-                {
-                    Console.WriteLine($"{item.Key}: {item.Value}");
-                }
-            }
-            else
-            {
-                Console.WriteLine($"Ошибка: {response.StatusCode}");
-            }
-        }
     }
 }
